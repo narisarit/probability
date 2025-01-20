b81e4665889378485ab7ac52ab859d049304f181
narisarit <rast777@naver.com>
2025-01-20 19:21:10 +0900

    new codes for probability

---
+++ probability.R
@@ -0,0 +1,25 @@
+sample()
+replicate()
+
+set.seed()
+
+expand.grid()
+
+permutations()
+combinations()
+
+duplicated()
+
+sapply()
+
+pnorm(a, mean, sd) #probability
+qnorm(p, mean, sd) #quantile
+dnorm() #density
+rnorm() #random number generation
+
+#student t distribution
+pt()
+qt()
+dt()
+rt()
+
